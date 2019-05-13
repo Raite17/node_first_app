7ce7af4be31f33e27342350fc7a9fb86beebd782
Raite17 <survivarian@gmail.com>
2019-05-13 19:34:33 +0600

    Add mongo connection and error handler

--- app.js
+++ app.js
@@ -6,10 +6,47 @@ const session = require('express-session');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const errorHandler = require('errorhandler');
+const config = require('./config');
 
 mongoose.promise = global.Promise;
 
+//middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(errorHandler());
+
+//Mongo connection
+mongoose.connection
+    .on("error", error => console.log(error))
+    .on("close", () => console.log("Database connection closed."))
+    .once("open", () => {
+        const info = mongoose.connections[0];
+        console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+    });
+
+mongoose.connect(config.MONGO_URL);
+
+//Выдаем 404 ошибку если не найдена страница
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+//Обрабатываем все возникшие ошибки
+app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.json({
+        errors: {
+            message: err.message,
+            error: {},
+        },
+    });
+});
+
+//Server start
+app.listen(config.PORT, () => {
+    console.log(`Сервер запущен по порту ${config.PORT}`);
+});
---
+++ config.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const root = path.join.bind(this, __dirname);
+dotenv.config({ path: root('.env') });
+
+//Экспортируем переменные окружения
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    MONGO_URL: process.env.MONGO_URL,
+}
